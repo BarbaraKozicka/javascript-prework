7948e8f7cb47da9a5a6d729dc3ecdd2d29672108
Barbara Kozicka <kozicka.b@gmail.com>
2021-02-02 21:51:47 +0100

    Add let funtion and printMessage to script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove = `papier`;
+printMessage('Zagrałem ' + playerMove + '! Wygrałem!')
